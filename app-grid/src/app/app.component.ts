f5dda3b389724d57301b52bcef79a80a5e844416
Neeraj Goswami <neerajpro@Neerajs-MacBook-Air.local>
2021-08-28 22:39:39 +0530

    Intial commit to ag-grid customisation

---
+++ app-grid/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+
+  columnDefs = [
+    { field: 'make', sortable: true, filter: true },
+    { field: 'model', sortable: true, filter: true },
+    { field: 'price', sortable: true, filter: true }
+  ];
+
+  public rowData: any = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get<any[]>('https://www.ag-grid.com/example-assets/small-row-data.json').subscribe(data => {
+      this.rowData = data;
+    })
+  }
+}
